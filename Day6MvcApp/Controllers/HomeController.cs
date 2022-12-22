a002111ad5f3366c4b073f495c9336f9d9db188b
Vajja Anuja <Vajja Anuja@anujavm>
2022-12-22 05:12:08 +0000

    Add project files.

---
+++ Day6MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using Day6MvcApp.Models;
+
+namespace Day6MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetServerTime()
+
+        {
+
+            System.Threading.Thread.Sleep(5000);
+
+            return PartialView();
+
+        }
+        static List<User> users = new List<User>();
+        public ActionResult GetAllUsers()
+
+        {
+
+            return PartialView(users);
+
+        }
+        public ActionResult CreateUser()
+
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+
+        public ActionResult CreateUser(User u)
+
+        {
+
+            users.Add(u);
+
+            return RedirectToAction("GetAllUsers");
+
+        }
+       public ActionResult Sum()
+        {
+            int num1 =2, num2 = 3;
+            int result = num1+ num2;
+            ViewBag.Total = result;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ Day6MvcApp/Models/Details.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day6MvcApp.Models
+{
+    public class Details
+    {
+        public int a { get; set; }
+    }
+}
